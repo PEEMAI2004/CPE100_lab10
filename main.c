8df94a4c1cd58b5a3a91cb822cfa5adbb4379e45
PEEMAI2004 <PEEMAI142016@GMAIL.COM>
2023-11-09 03:05:33 +0700

    Bugs fix
    Temporary remove avg grade of subject groups

--- main.c
+++ main.c
@@ -2,156 +2,142 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_GRADES 100
 #define MAX_SUBJECTS 100
-// struct to store subject data
-typedef struct
-{
-  char code[50];
-  char subjectName[50];
-  float cr;
-  float grade;
-} Subject;
-// struct to store grade data
-typedef struct
-{
-  char grade[50];
-  Subject subjects[MAX_SUBJECTS];
-  int numSubjects;
-  float avgGrade;
-} GradeData;
-// struct to store subject group data
-typedef struct
-{
-  char subjectGroup[3];
-  float totalGrade;
-  int count;
-} SubjectGroup;
+#define MAX_TERMS 10
+#define MAX_LINE_LENGTH 100
 
-int main()
-{
-  // change file name here
-  char filename[] = "grades.csv";
-  FILE *file, *outputFile;
-  char line[100];
-  GradeData grades[MAX_GRADES];
-  int numGrades = 0;
-  float totalGradeSum = 0;
-  int totalNumSubjects = 0;
-  SubjectGroup subjectGroups[MAX_SUBJECTS];
-  int numSubjectGroups = 0;
-  int isFirstLine = 1;
+struct Subject {
+    char code[20];
+    float credit;
+    float grade;
+};
 
-  file = fopen(filename, "r");
-  // in case file does not exist
-  if (file == NULL)
-  {
-    perror("Error opening file");
-    return -1;
-  }
-  // create output file
-  outputFile = fopen("output.csv", "w");
-  // in case file does not exist
-  if (outputFile == NULL)
-  {
-    perror("Error creating output file");
-    return -1;
-  }
-  // read file line by line
-  while (fgets(line, sizeof(line), file))
-  {
-    // skip first line because it is the header
-    if (isFirstLine)
-    {
-      isFirstLine = 0;
-      continue;
+struct Term {
+    char name[50];
+    struct Subject subjects[MAX_SUBJECTS];
+    int num_subjects;
+    float total_credits;
+};
+
+struct Term terms[MAX_TERMS];
+int num_terms = 0;
+
+void readGradesFromFile(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        exit(1);
     }
-    // if line does not start with a comma, it is a new grade
-    if (line[0] != ',')
-    {
-      sscanf(line, "%[^,],", grades[numGrades].grade); // read grade
-      grades[numGrades].numSubjects = 0;               // reset number of subjects
-      numGrades++;                                     // increment number of grades
-    }
-    else
-    {
-      // if line starts with a comma, it is a new subject
-      Subject newSubject; // create new subject
-      sscanf(line, ",%[^,],%[^,],%f,%f", newSubject.code,
-             newSubject.subjectName, &newSubject.cr, &newSubject.grade); // read subject data
-      strcpy(grades[numGrades - 1]
-                 .subjects[grades[numGrades - 1].numSubjects]
-                 .code,
-             newSubject.code); // copy subject data to grade data
-      strcpy(grades[numGrades - 1]
-                 .subjects[grades[numGrades - 1].numSubjects]
-                 .subjectName,
-             newSubject.subjectName); // copy subject data to grade data
-      grades[numGrades - 1].subjects[grades[numGrades - 1].numSubjects].cr =
-          newSubject.cr; // copy subject data to grade data
-      grades[numGrades - 1].subjects[grades[numGrades - 1].numSubjects].grade =
-          newSubject.grade;                // copy subject data to grade data
-      grades[numGrades - 1].numSubjects++; // increment number of subjects
-      totalGradeSum += newSubject.grade;   // calculate total grade sum
-      totalNumSubjects++;                  // calculate total number of subjects
 
-      // Calculate subject group averages
-      char subjectGroup[3];
-      strncpy(subjectGroup, newSubject.code, 2); // get first 2 characters of subject code
-      subjectGroup[2] = '\0';                    // add null terminator
-      int found = 0;
-      for (int i = 0; i < numSubjectGroups; i++) // check if subject group already exists
-      {
-        if (strcmp(subjectGroup, subjectGroups[i].subjectGroup) == 0) // if subject group exists
-        {
-          // add grade to total grade and increment count
-          subjectGroups[i].totalGrade += newSubject.grade; // add grade to total grade
-          subjectGroups[i].count++;                        // increment count
-          found = 1;                                       // set found to true
-          break;
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        if (line[0] == '\n' || line[0] == '\0') {
+            continue;  // Skip empty lines
         }
-      }
-      if (!found) // if subject group does not exist
-      {
-        strcpy(subjectGroups[numSubjectGroups].subjectGroup, subjectGroup); // copy subject group to subject group data
-        subjectGroups[numSubjectGroups].totalGrade = newSubject.grade;      // set total grade to grade
-        subjectGroups[numSubjectGroups].count = 1;                          // set count to 1
-        numSubjectGroups++;                                                 // increment number of subject groups
-      }
-    }
-  }
-  // calculate average grade for each grade
-  for (int i = 0; i < numGrades; i++) 
-  {
-    // calculate average grade
-    float sum = 0;
-    for (int j = 0; j < grades[i].numSubjects; j++)
-    {
-      sum += grades[i].subjects[j].grade; // add grade to sum
-    }
-    grades[i].avgGrade = sum / grades[i].numSubjects; // calculate average grade
-  }
 
-  // write to output file
-  // write grade data
-  fprintf(outputFile, "Grade,Average Grade\n"); // write header
-  for (int i = 0; i < numGrades; i++) // write grade data
-  {
-    fprintf(outputFile, "%s,%.2f\n", grades[i].grade, grades[i].avgGrade);
-  }
-  // write overall average grade
-  fprintf(outputFile, "Overall Average Grade,%.2f\n",
-          totalGradeSum / totalNumSubjects);
-  // write subject group averages
-  fprintf(outputFile, "\nSubject Group,Average Grade\n");
-  for (int i = 0; i < numSubjectGroups; i++) // write subject group data
-  {
-    fprintf(outputFile, "%s,%.2f\n", subjectGroups[i].subjectGroup,
-            subjectGroups[i].totalGrade / subjectGroups[i].count);
-  }
-  
-  fclose(file); // close input file
-  fclose(outputFile); // close output file
+        if (line[0] != ',') {
+            // New term
+            if (num_terms < MAX_TERMS) {
+                sscanf(line, "%[^,],", terms[num_terms].name);
+                terms[num_terms].num_subjects = 0;
+                terms[num_terms].total_credits = 0.0;
+                num_terms++;
+            } else {
+                printf("Too many terms, ignoring the rest.\n");
+                break;
+            }
+        } else {
+            // Subject data
+            if (num_terms > 0) {
+                if (terms[num_terms - 1].num_subjects < MAX_SUBJECTS) {
+                    sscanf(line, ",%[^,],%*[^,],%f,%f", terms[num_terms - 1].subjects[terms[num_terms - 1].num_subjects].code,
+                           &terms[num_terms - 1].subjects[terms[num_terms - 1].num_subjects].credit,
+                           &terms[num_terms - 1].subjects[terms[num_terms - 1].num_subjects].grade);
+                    terms[num_terms - 1].num_subjects++;
+                    terms[num_terms - 1].total_credits += terms[num_terms - 1].subjects[terms[num_terms - 1].num_subjects - 1].credit;
+                } else {
+                    printf("Too many subjects in the current term, ignoring the rest.\n");
+                }
+            }
+        }
+    }
 
-  return 0;
+    fclose(file);
+}
+
+void printAllGrades() {
+    for (int i = 0; i < num_terms; i++) {
+        printf("%s\n", terms[i].name);
+        for (int j = 0; j < terms[i].num_subjects; j++) {
+            printf("Subject Code: %s, Credit: %.1f, Grade: %.1f\n",
+                   terms[i].subjects[j].code, terms[i].subjects[j].credit, terms[i].subjects[j].grade);
+        }
+    }
+}
+
+void printAvgGradeOfEachTerm() {
+    for (int i = 0; i < num_terms; i++) {
+        float total_grade = 0.0;
+        for (int j = 0; j < terms[i].num_subjects; j++) {
+            total_grade += terms[i].subjects[j].grade * terms[i].subjects[j].credit;
+        }
+        float avg_grade = total_grade / terms[i].total_credits;
+        printf("Term: %s, Average Grade: %.2f\n", terms[i].name, avg_grade);
+    }
+}
+
+void printOverallAvgGrade() {
+    float overall_total_grade = 0.0;
+    float overall_total_credits = 0.0;
+
+    for (int i = 0; i < num_terms; i++) {
+        for (int j = 0; j < terms[i].num_subjects; j++) {
+            overall_total_grade += terms[i].subjects[j].grade * terms[i].subjects[j].credit;
+            overall_total_credits += terms[i].subjects[j].credit;
+        }
+    }
+
+    float overall_avg_grade = overall_total_grade / overall_total_credits;
+    printf("Overall Average Grade: %.2f\n", overall_avg_grade);
+}
+void writeOutputToCSV() {
+    FILE *file = fopen("output.csv", "w");
+    if (file == NULL) {
+        printf("Failed to create the output file.\n");
+        exit(1);
+    }
+
+    fprintf(file, "Average Grade of Each Term:\n");
+    for (int i = 0; i < num_terms; i++) {
+        float total_grade = 0.0;
+        for (int j = 0; j < terms[i].num_subjects; j++) {
+            total_grade += terms[i].subjects[j].grade * terms[i].subjects[j].credit;
+        }
+        float avg_grade = total_grade / terms[i].total_credits;
+        fprintf(file, "%s,%.2f\n", terms[i].name, avg_grade);
+    }
+
+    fprintf(file, "\nOverall Average Grade:\n");
+    float overall_total_grade = 0.0;
+    float overall_total_credits = 0.0;
+    for (int i = 0; i < num_terms; i++) {
+        for (int j = 0; j < terms[i].num_subjects; j++) {
+            overall_total_grade += terms[i].subjects[j].grade * terms[i].subjects[j].credit;
+            overall_total_credits += terms[i].subjects[j].credit;
+        }
+    }
+    float overall_avg_grade = overall_total_grade / overall_total_credits;
+    fprintf(file, "%.2f\n", overall_avg_grade);
+
+    fclose(file);
+}
+
+
+int main() {
+    const char *filename = "grades.csv";
+    readGradesFromFile(filename);
+
+    writeOutputToCSV();
+
+    return 0;
 }
